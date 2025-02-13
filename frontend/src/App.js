69175291239f812847835c4fb947ee53a6ef857f
Ansh <anshbansal28@gmail.com>
2025-02-13 15:02:07 +0530

    basic frontend and backend connected

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then((res) => res.text())
+      .then((data) => setMessage(data));
+  }, []);
+
+  return (
+    <div className="flex items-center justify-center h-screen text-xl font-bold">
+      {message || "Loading..."}
+    </div>
+  );
+}
+
+export default App;
+
